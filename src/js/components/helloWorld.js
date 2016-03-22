f34b90da0b1ab6bdc1469fe9df47881d88035a44
Kirk <kkchu791@gmail.com>
2016-03-22 13:55:01 -0700

    Add random output feature

--- src/js/components/helloWorld.js
+++ src/js/components/helloWorld.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import axios from 'axios'
-import animate from 'animate.css'
 
 export default class HelloWorld extends Component {
   constructor() {
@@ -22,14 +21,14 @@ export default class HelloWorld extends Component {
       return (
         <div>
           <button className='btn-warning' onClick={function(){this.onPress(); this.getResponse()}.bind(this)}>Shake</button>
-          <h1>{this.state.response.map(e => e['attributes']['reply'])[0]}</h1>
+          <h1>{this.state.response.map(e => e['attributes']['reply'])[Math.floor((Math.random() * 20) + 1)]}</h1>
         </div>
       )
     } else {
       return (
         <div>
-          <button className='btn-warning' onClick={function(){this.onPress(); this.getResponse()}.bind(this)}>Shake Again</button>
-          <h1>{this.state.response.map(e => e['attributes']['reply'])[1]}</h1>
+          <button className='btn-warning' onClick={function(){this.onPress(); this.getResponse()}.bind(this)}>Shake</button>
+          <h1>{this.state.response.map(e => e['attributes']['reply'])[Math.floor((Math.random() * 20) + 1)]}</h1>
         </div>
       )
     }
@@ -44,7 +43,7 @@ export default class HelloWorld extends Component {
     return (
       <div className="container" style={{maxWidth: 800, width:800, height: 1000, marginLeft: 'auto', marginRight: 'auto', marginTop: 10, textAlign:'center'}}>
         {this.renderAction()}
-        <img className="bounce" style={{paddingTop:90,width:700, height: 550, margin:0, display: 'block', paddingRight: 120}}src="https://pixabay.com/static/uploads/photo/2012/04/16/11/05/ball-35516_960_720.png"/>
+        <img style={{paddingTop:90,width:700, height: 550, margin:0, display: 'block', paddingRight: 120}}src="https://pixabay.com/static/uploads/photo/2012/04/16/11/05/ball-35516_960_720.png"/>
       </div>
     )
   }
